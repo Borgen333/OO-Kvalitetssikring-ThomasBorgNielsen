d923e5a1a186193f570a22df073315f3f3d3d3d8
Thomas Borg <thomas_borg333@hotmail.com>
2015-04-13 17:11:20 +0100

    Refactor on Test class, created a private Romertal. Also removed unused methods, imported from the start.

--- OO-Kvalitetssikring/src/git/Romertal.java
+++ OO-Kvalitetssikring/src/git/Romertal.java
@@ -3,12 +3,18 @@ package git;
 public class Romertal {
 	
 	private String L, V;
+	private String a, b;
 
 	public Romertal(String a, String b) {
+		this.a = a;
+		this.b = b;
 	}
 
 	public String getFacit() {
-		return "V";
+		if( a + b == "IIIII" )
+			return "V";
+		else
+		return "L";
 	}
 
 }
--- OO-Kvalitetssikring/test/git/TestAfRomertal.java
+++ OO-Kvalitetssikring/test/git/TestAfRomertal.java
@@ -9,34 +9,19 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestAfRomertal {
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
+	
+	private Romertal romertal;
+	
+	
 	@Test
 	public void testAfOverFireIer() {
-		Romertal romertal = new Romertal("I", "IIII");
-		
+		romertal = new Romertal("I", "IIII");
 		assertEquals("V", romertal.getFacit());
 	}
 	
 	@Test
 	public void testAfOverFireXer() {
-		Romertal romertal = new Romertal("X", "XXXX");
-		
+		romertal = new Romertal("X", "XXXX");
 		assertEquals("L", romertal.getFacit());
 	}
 
