c8d57b9b356b2ce791e5f7803ccc828c12dd3168
Thomas Borg <thomas_borg333@hotmail.com>
2015-04-15 20:13:23 +0100

    Created 2 new Strings in Romertal class. Still red bar.

--- OO-Kvalitetssikring/src/git/Romertal.java
+++ OO-Kvalitetssikring/src/git/Romertal.java
@@ -2,7 +2,12 @@ package git;
 
 public class Romertal {
 
+	private String b;
+	private String a;
+
 	public Romertal(String a, String b) {
+		this.a = a;
+		this.b = b;
 	}
 
 	public String getFacit() {
