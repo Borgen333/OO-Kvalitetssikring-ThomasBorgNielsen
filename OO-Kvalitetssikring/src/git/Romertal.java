c11284f5b58630015e074d890ed6ca273add34e6
Thomas Borg <thomas_borg333@hotmail.com>
2015-04-16 10:11:12 +0100

    Trying a new approach. created Stringbuilder sbI and sbX. Still red bar.

--- OO-Kvalitetssikring/src/git/Romertal.java
+++ OO-Kvalitetssikring/src/git/Romertal.java
@@ -4,14 +4,14 @@ public class Romertal {
 
 	private String b;
 	private String a;
+	private String c;
+	private StringBuilder sbI = new StringBuilder("");
+	private StringBuilder sbX = new StringBuilder("");
+	private StringBuilder sb = new StringBuilder();
 
 	public Romertal(String a, String b) {
 		this.a = a;
 		this.b = b;
+		c = a + b;
 	}
-
-	public String getFacit() {
-		return "V";
-	}
-
 }
