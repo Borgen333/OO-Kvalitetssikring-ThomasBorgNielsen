5b14a9f62f3e5e18e5d3ae9afc9e2d4cca6906d8
Thomas Borg <thomas_borg333@hotmail.com>
2015-04-16 10:17:10 +0100

    Added new StringBuilder: samletSB working on green bar for test case - 1.

--- OO-Kvalitetssikring/src/git/Romertal.java
+++ OO-Kvalitetssikring/src/git/Romertal.java
@@ -7,11 +7,22 @@ public class Romertal {
 	private String c;
 	private StringBuilder sbI = new StringBuilder("");
 	private StringBuilder sbX = new StringBuilder("");
+	private StringBuilder samletSB = new StringBuilder("");
 	private StringBuilder sb = new StringBuilder();
 
 	public Romertal(String a, String b) {
 		this.a = a;
 		this.b = b;
 		c = a + b;
+	}
+	
+	public String getFacit() {
+		
+		for(int i = 0; i < c.length(); i++){
+			if(c.charAt(i) == 'I')
+				sbI.append("I");
+		}
+		return samletSB.append(sbX).append(sbI).toString();
+		
 	}
 }
