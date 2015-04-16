1a46b721401278ea01ad6dfa566cee11fb848aa4
Thomas Borg <thomas_borg333@hotmail.com>
2015-04-16 10:23:43 +0100

    Archived Green bar for test case 2 aswell as case 1. moving on to test case 3

--- OO-Kvalitetssikring/src/git/Romertal.java
+++ OO-Kvalitetssikring/src/git/Romertal.java
@@ -8,26 +8,31 @@ public class Romertal {
 	private StringBuilder sbI = new StringBuilder("");
 	private StringBuilder sbX = new StringBuilder("");
 	private StringBuilder sbV = new StringBuilder("");
+	private StringBuilder sbL = new StringBuilder("");
 	private StringBuilder samletSB = new StringBuilder("");
-	private StringBuilder sb = new StringBuilder();
 
 	public Romertal(String a, String b) {
 		this.a = a;
 		this.b = b;
 		c = a + b;
 	}
-	
+
 	public String getFacit() {
-		
-		for(int i = 0; i < c.length(); i++){
-			if(c.charAt(i) == 'I')
+
+		for (int i = 0; i < c.length(); i++) {
+			if (c.charAt(i) == 'I')
 				sbI.append("I");
-			if(sbI.indexOf("IIIII") == 0){
+			if (sbI.indexOf("IIIII") == 0) {
 				sbI.delete(0, 5);
-				sbV .append("V");
-			}
-		}
-		return samletSB.append(sbX).append(sbV).append(sbI).toString();
-		
+				sbV.append("V");
+			}}
+		for (int i = 0; i < c.length(); i++) {
+			if (c.charAt(i) == 'X')
+				sbX.append("X");
+			if (sbX.indexOf("XXXXX") == 0) {
+				sbX.delete(0, 5);
+				sbL.append("L");
+			}}
+		return samletSB.append(sbL).append(sbX).append(sbV).append(sbI).toString();
 	}
 }
